a962e3c0da63b41c5dc5a68160eef4895cbc2121
leserpent <leserpent>
2004-04-01 20:39:06 +0000

    Useless

--- pou/PoUgui/src/mc/mctest.cc
+++ pou/PoUgui/src/mc/mctest.cc
@@ -2,21 +2,26 @@
 
 #include "math/vector3.h"
 #include "ImplicitSurface3D.h"
+#include "mc2.h"
 #include "mc.h"
 using namespace std;
 
 
 int main(int argc, char *argv) {
+//  Mc mc;
   vector<Vec3f> vertices, normals;
   vector<unsigned int> indices;
   
-  ImplicitSurface3D is(ConstructRBFPOU::BIHARMONIC);
-  PointSet ps;
-  ps.load("dragon-soft.sur");
-  is.compute(ps, 3000);
-  is.save("test.pou");
-//  ImplicitSurface3D is;
-//  is.load("test.pou");
+//  ImplicitSurface3D is(ConstructRBFPOU::BIHARMONIC);
+//  PointSet ps;
+//  ps.load("dragon-soft.sur");
+//  is.compute(ps, 3000);
+//  is.save("test.pou");
+  ImplicitSurface3D is;
+  is.load("test.pou");
+//  makecubetable();
+//  mc.makecubetable();
+//  mc.domc(&is);
 //   domc(&is, );
 //   getVertNorm(vertices, normals);
 //   indices = getIndices();
