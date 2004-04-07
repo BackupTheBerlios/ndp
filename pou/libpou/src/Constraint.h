533715661c5ac846ccf556b98678d47e39324db9
leserpent <leserpent>
2004-04-07 08:02:30 +0000

    Added a	default value to the parameter param of the constructor

--- pou/libpou/src/Constraint.h
+++ pou/libpou/src/Constraint.h
@@ -6,6 +6,9 @@
  * @brief  One Constraint management
  *
  * $Log: Constraint.h,v $
+ * Revision 1.4  2004/04/07 08:02:30  leserpent
+ * Added a	default value to the parameter param of the constructor
+ *
  * Revision 1.3  2004/04/05 19:14:36  pumpkins
  * File documentation
  *
@@ -22,7 +25,7 @@ class Constraint {
   Vec3f v;
   float c;
  public:
-  Constraint(const Vec3f& _v, const float _c) : v(_v), c(_c) {}
+  Constraint(const Vec3f& _v, const float _c = 0) : v(_v), c(_c) {}
 
   Vec3f getVector() const {
     return v;
