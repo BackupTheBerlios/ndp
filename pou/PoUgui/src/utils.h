989d5977feaef4ebbd0d3ce3dc1c580510c37ecf
leserpent <leserpent>
2004-04-28 18:02:52 +0000

    Changed all ?: to clamp()

--- pou/PoUgui/src/utils.h
+++ pou/PoUgui/src/utils.h
@@ -9,6 +9,12 @@ class QWidget;
 template<typename T>
 T deg2rad(T x) { return (x*M_PI)/T(180); }
 
+// Clamp x into [a,b] 
+template<typename T>
+T clamp(T x, T a, T b) {
+  return std::min(std::max(a, x), b);
+}
+
 void CheckConfigPath();
 QString getConfigPath();
 void CheckOpenGL();
